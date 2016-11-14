e6b03347ae494c9bedd09ebbed7e62bd43db20eb
Quarks0 <bdippelsman10@gmail.com>
2016-11-13 17:03:53 -0800

    Added some base text and domcontentloaded

---
+++ pathfinder.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<h1>Hello from React</h1>, document.getElementById('root'));
+});
