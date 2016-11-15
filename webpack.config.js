95f3d6f8f094f594eb11bc20600ae361049387b8
Quarks0 <bdippelsman10@gmail.com>
2016-11-15 10:06:41 -0800

    Started rendering of grid

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require("path");
 
 module.exports = {
   context: __dirname,
-  entry: "./pathfinder.js",
+  entry: "./lib/pathfinder.js",
   output: {
     path: path.join(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
